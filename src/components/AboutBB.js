ea85d35c74966d1c159a9896963914bb72e08e01
Aman Singal <amansingal6@gmail.com>
2020-07-12 17:07:36 +0530

    Fixed BB

--- src/components/AboutBB.js
+++ src/components/AboutBB.js
@@ -10,11 +10,8 @@ function AboutBB() {
         <div className="block-example border border-dark" style={{width: '1140px' , paddingLeft: '15px' , paddingRight: '15px' }}>
         <div style={{marginTop: "20px" , marginBottom: "20px" , paddingRight:"15px" }}>
             <h2 style={{fontSize: '30px'}}>bigbasket – online grocery store</h2>
-        <p style={{fontSize: '14px'}}>Did you ever imagine that the freshest of fruits and vegetables, top quality pulses and food grains, dairy products and hundreds of branded items could be handpicked and delivered to your home, all at the click of a button? India’s first comprehensive online megastore, bigbasket.com, brings a whopping 20000+ products with more than 1000 brands, to over 4 million happy customers. From household cleaning products to beauty and makeup, bigbasket has everything you need for your daily needs. bigbasket.com is convenience personified We’ve taken away all the stress associated with shopping for daily essentials, and you can now order all your household products and even buy groceries online without travelling long distances or standing in serpentine queues. Add to this the convenience of finding all your requirements at one single source, along with great savings, and you will realize that bigbasket- India’s largest online supermarket, has revolutionized the way India shops for groceries. Online grocery shopping has never been easier. Need things fresh? Whether it’s fruits and vegetables or dairy and meat, we have this covered as well! Get fresh eggs, meat, fish and more online at your convenience. Hassle-free Home Delivery options </p>
-      
+        <p style={{fontSize: '14px' , Color: '#333'}}>Did you ever imagine that the freshest of fruits and vegetables, top quality pulses and food grains, dairy products and hundreds of branded items could be handpicked and delivered to your home, all at the click of a button? India’s first comprehensive online megastore, bigbasket.com, brings a whopping 20000+ products with more than 1000 brands, to over 4 million happy customers. From household cleaning products to beauty and makeup, bigbasket has everything you need for your daily needs. bigbasket.com is convenience personified We’ve taken away all the stress associated with shopping for daily essentials, and you can now order all your household products and even buy groceries online without travelling long distances or standing in serpentine queues. Add to this the convenience of finding all your requirements at one single source, along with great savings, and you will realize that bigbasket- India’s largest online supermarket, has revolutionized the way India shops for groceries. Online grocery shopping has never been easier. Need things fresh? Whether it’s fruits and vegetables or dairy and meat, we have this covered as well! Get fresh eggs, meat, fish and more online at your convenience. Hassle-free Home Delivery options </p>
         <p style={{fontSize: '14px'}}>We deliver to 25 cities across India and maintain excellent delivery times, ensuring that all your products from groceries to snacks branded foods reach you in time.</p> 
-          
-        <br />
         <ul>
             <li style={{fontSize: '14px'}}> Slotted Delivery: Pick the most convenient delivery slot to have your grocery delivered. From early morning delivery for early birds, to late-night delivery for people who work the late shift, bigbasket caters to every schedule.</li>
             <li style={{fontSize: '14px'}}>Express Delivery: This super useful service can be availed by customers in cities like Bangalore, Mumbai, Pune, Chennai, Kolkata, Hyderabad and Delhi-NCR in which we deliver your orders to your doorstep in 90 Minutes.</li>
@@ -22,13 +19,14 @@ function AboutBB() {
         </ul>
         </div>
 
-        {open ? null :  <Button
+        {open ? null :  <button
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open}
+        style={{width: '85px' , height: '21px'}}
       >
-        Show More
-      </Button> }
+        <p style={{fontSize: '12px'}}> Read More...</p>
+      </button> }
      
       <Collapse in={open}>
         <div style={{paddingRight: '15px'}}>
